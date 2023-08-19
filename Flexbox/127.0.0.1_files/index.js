e1dfa4171fc4745c4ceb3ce5e11b42c2970a59c4
[saurabh rao] <[saurabhraogkp@gmail.com]>
2023-08-19 15:01:29 +0530

    basic html & css tutorials

---
+++ Flexbox/127.0.0.1_files/index.js
@@ -0,0 +1,7 @@
+const words = document.getElementById(noOfWords)
+
+const arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const generateWords = (words) => {
+    console.log(Number(Math.random()).toFixed(0) * 25)
+}
